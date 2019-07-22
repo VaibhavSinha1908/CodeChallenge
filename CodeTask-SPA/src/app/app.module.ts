8169000356c500e47947bab5f01dc0ae58d2b578
Vaibhav Sinha <43650684+VaibhavSinha1908@users.noreply.github.com>
2019-07-23 02:24:09 +1000

    Adding Header and Footer; custom styles

--- CodeTask-SPA/src/app/app.module.ts
+++ CodeTask-SPA/src/app/app.module.ts
@@ -4,11 +4,15 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FormComponent } from './form/form.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
    declarations: [
       AppComponent,
-      FormComponent
+      FormComponent,
+      HeaderComponent,
+      FooterComponent
    ],
    imports: [
       BrowserModule,
